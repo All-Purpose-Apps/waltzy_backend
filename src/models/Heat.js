312097e4c14f023eed38fce4edcf95ea2148c6a0
joshpled <joshuapleduc@gmail.com>
2024-04-22 19:30:40 -0400

    limit couples to 8 in heat schema

--- src/models/Heat.js
+++ src/models/Heat.js
@@ -4,12 +4,17 @@ const heatSchema = new mongoose.Schema({
   couples: {
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Couple' }],
     required: true,
+    validate: [arrayLimit, '{PATH} exceeds the limit of 8'],
   },
   competitions: {
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dance' }],
     required: true,
   },
 });
+
+function arrayLimit(val) {
+  return val.length <= 8;
+}
 
 const Heat = mongoose.model('Heat', heatSchema);
 
