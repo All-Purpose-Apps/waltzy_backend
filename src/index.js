a95f0a045b0b77423072802f69719f5212fa2637
joshpled <joshuapleduc@gmail.com>
2024-09-04 21:13:16 -0400

    console log change in index, for lport listening

--- src/index.js
+++ src/index.js
@@ -48,5 +48,5 @@ router(app);
 // expressListRoutes(app);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Express server started at port: ${PORT}`);
+  console.log(`Express server started at port: ${process.env.PORT || 3000}`);
 });
